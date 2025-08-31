7bdc181a669d072af7d91dfc82138a68ae31f8c3
Hamza <ghafarhamzaamsn.777@gmail.com>
2025-08-31 20:22:14 +0300

    create home view

---
+++ lib/Features/Home/presentation/Views/Home_View.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/Features/Home/presentation/Views/Widgets/Home_View_Body.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: HomeViewBody());
+  }
+}
