2abbe9dcb31d25c2702e5eeaec0d6d420ea64891
Hamza <ghafarhamzaamsn.777@gmail.com>
2025-09-12 16:12:18 +0300

    Provide Bloc Provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/Core/consts.dart';
 import 'package:notes_app/Features/Home/data/Models/note.dart';
+import 'package:notes_app/Features/Home/presentation/Manager/Add_note_cubit/Add_note_cubit.dart';
 import 'package:notes_app/Features/Home/presentation/Views/Home_View.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -16,10 +18,13 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: const HomeView(),
-      debugShowCheckedModeBanner: false,
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => AddNoteCubit())],
+      child: MaterialApp(
+        theme: ThemeData.dark(),
+        home: const HomeView(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
